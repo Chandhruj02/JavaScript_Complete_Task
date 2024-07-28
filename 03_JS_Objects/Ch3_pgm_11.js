08974d984134fa4dbac40cde61aecad896790638
Chandhruj02 <chandhruj02@gmail.com>
2024-07-28 18:24:34 +0530

    Update Ch3_pgm_11.js

--- 03_JS_Objects/Ch3_pgm_11.js
+++ 03_JS_Objects/Ch3_pgm_11.js
@@ -1,20 +1,26 @@
 // Using a property in a calculation
 
 var player1;
-
 player1 = {
 	  name: "Max",
 	  score: 0
 };
-
+console.log(player1.name + " has scored " + player1.score);
+player1.score = player1.score + 20;
 console.log(player1.name + " has scored " + player1.score);
 
-player1.score = player1.score + 50;
+var player2;
+player2 = {
+	  name: "Chandhru",
+	  score: 0
+};
+console.log(player2.name + " has scored " + player2.score);
+player2.score = player2.score + 50;
+console.log(player2.name + " has scored " + player2.score);
+console.log(player2.score);
 
-console.log(player1.name + " has scored " + player1.score);
-
-
-
+var sum = player1.score + player2.score;
+console.log(sum);
 /* Further Adventures
  *
  * 1) Write code to increase player1's score by 10%.
