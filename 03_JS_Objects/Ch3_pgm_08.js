412301a782a1cde295b5e76c954075d600ac20c7
Chandhruj02 <chandhruj02@gmail.com>
2024-07-28 18:14:37 +0530

    Update Ch3_pgm_08.js

--- 03_JS_Objects/Ch3_pgm_08.js
+++ 03_JS_Objects/Ch3_pgm_08.js
@@ -3,15 +3,26 @@
 var book;
 
 book = {
-  title     : "The Hobbit",
-	author    : "J. R. R. Tolkien",
-	published : 1937
+   title : "The Hobbit",
+   author : "J. R. R. Tolkien",
+   published : 1937
 };
 
 console.log(book.title);
 console.log(book.author);
+console.log(book.published);
 
+var book2;
 
+book2 = {
+   title : "The Kings of Dance",
+   author : "J. K. Rog",
+   published : 1950
+};
+
+console.log(book2.title);
+console.log(book2.author);
+console.log(book2.published);
 
 /* Further Adventures
  *
