d9f962e6b64b419b6f55c97f58e4a5b49730b015
Chandhruj02 <chandhruj02@gmail.com>
2024-07-28 18:15:50 +0530

    Update Ch3_pgm_09.js

--- 03_JS_Objects/Ch3_pgm_09.js
+++ 03_JS_Objects/Ch3_pgm_09.js
@@ -13,8 +13,14 @@ book2 = {
 	author: "Philip Pullman"
 };
 
+book3 = {
+	title: "The kings of titans",
+	author: "Chandhru J"
+};
+
 console.log(book1.title + " by " + book1.author);
 console.log(book2.title + " by " + book2.author);
+console.log(book3.title + " by " + book3.author);
 
 
 
