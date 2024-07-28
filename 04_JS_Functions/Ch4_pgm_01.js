7c2ed957fa88b393e0e49ce9e418a411ccb48f72
Chandhruj02 <chandhruj02@gmail.com>
2024-07-28 18:37:50 +0530

    Update Ch4_pgm_01.js

--- 04_JS_Functions/Ch4_pgm_01.js
+++ 04_JS_Functions/Ch4_pgm_01.js
@@ -14,6 +14,18 @@ console.log("Actors: " + movie1.actors);
 console.log("Directors: " + movie1.directors);
 console.log("------------------------------");
 
+var movie2;
+movie2 = {
+  title: "Tamilan Cars 2",
+  actors: "Cars Hero, Cars Competitor",
+  directors: "Chandhru J"
+};
+
+console.log("Movie information for " + movie2.title);
+console.log("------------------------------");
+console.log("Actors: " + movie2.actors);
+console.log("Directors: " + movie2.directors);
+console.log("------------------------------");
 
 
 /* Further Adventures
