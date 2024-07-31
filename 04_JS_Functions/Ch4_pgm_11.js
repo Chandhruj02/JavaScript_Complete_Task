cb5fbac6ea4318f7f1df6c92e369e125e69a2bca
Chandhruj02 <chandhruj02@gmail.com>
2024-07-31 07:52:25 +0530

    Update Ch4_pgm_11.js

--- 04_JS_Functions/Ch4_pgm_11.js
+++ 04_JS_Functions/Ch4_pgm_11.js
@@ -1,47 +1,83 @@
 // Using the same function with multiple objects
 
-var movie1;
-var movie2;
-var movie3;
-var movie;
-var showMovieInfo;
+// var movie1;
+// var movie2;
+// var movie3;
+// var movie;
+// var showMovieInfo;
 
-movie1 = {
-    title: "Inside Out",
-    actors: "Amy Poehler, Bill Hader",
-    directors: "Pete Doctor, Ronaldo Del Carmen"
+// movie1 = {
+//     title: "Inside Out",
+//     actors: "Amy Poehler, Bill Hader",
+//     directors: "Pete Doctor, Ronaldo Del Carmen"
+// };
+
+// movie2 = {
+//     title: "Spectre",
+//     actors: "Daniel Craig, Christoph Waltz",
+//     directors: "Sam Mendes"
+// };
+
+// movie3 = {
+//     title: "Star Wars: Episode VII - The Force Awakens",
+//     actors: "Harrison Ford, Mark Hamill, Carrie Fisher",
+//     directors: "J.J.Abrams"
+// };
+
+// showMovieInfo = function () {
+//     console.log("Movie information for " + movie.title);
+//     console.log("------------------------------");
+//     console.log("Actors: " + movie.actors);
+//     console.log("Directors: " + movie.directors);
+//     console.log("------------------------------");
+// };
+
+// movie = movie1;
+// showMovieInfo();
+
+// movie = movie2;
+// showMovieInfo();
+
+// movie = movie3;
+// showMovieInfo();
+
+//<------ANSWER------>
+var Quiz1;
+var Quiz2;
+var showQuestion;
+
+Quiz1 = {
+    Q : "Which of the following Language is used for Front-end Development",
+	1 : "C++",
+    2 : "Python",
+    3 : "Java Script",
+	4 : "None of the above"
 };
 
-movie2 = {
-    title: "Spectre",
-    actors: "Daniel Craig, Christoph Waltz",
-    directors: "Sam Mendes"
+Quiz2 = {
+    Q : "Which of the following Language is used for Designing UI",
+	1 : "Corel Draw",
+    2 : "Figma",
+    3 : "PhotoShop",
+	4 : "None of the above"
 };
 
-movie3 = {
-    title: "Star Wars: Episode VII - The Force Awakens",
-    actors: "Harrison Ford, Mark Hamill, Carrie Fisher",
-    directors: "J.J.Abrams"
-};
-
-showMovieInfo = function () {
-    console.log("Movie information for " + movie.title);
+showQuestion = function () {
+    console.log("Question" + Quiz.Q);
     console.log("------------------------------");
-    console.log("Actors: " + movie.actors);
-    console.log("Directors: " + movie.directors);
+    console.log("Options are");
+    console.log("A)" + Quiz[1]);
+	console.log("B)" + Quiz[2]);
+	console.log("C)" + Quiz[3]);
+	console.log("D)" + Quiz[4]);
     console.log("------------------------------");
 };
 
-movie = movie1;
-showMovieInfo();
+Quiz = Quiz1;
+showQuestion();
 
-movie = movie2;
-showMovieInfo();
-
-movie = movie3;
-showMovieInfo();
-
-
+Quiz = Quiz2;
+showQuestion();
 
 /* Further Adventures
  *
